225705e4913f2c84f2dc0cc3aaf6350381acff5b
Gelidus <gelidus@hotmail.sk>
2015-03-09 19:59:32 +0100

    Added statistics at the end of test

--- test.php
+++ test.php
@@ -7,6 +7,9 @@
 	$results = array_diff(scandir($testDir . "results"), array('..', '.'));
 	include($testDir . "commands.php");
 
+	$total = count($commands);
+	$good = 0; $bad = 0;
+
 	foreach ($commands as $name => $opts) {
 		$jsonName = $name . ".json";
 		$xmlName = $name . ".xml";
@@ -20,9 +23,11 @@
 
 		if ($returnCode != $code) { # code from commands
 			echo "[ERR] Test " . $name . " failed with code: " . $returnCode . " expected: " . $code . "\n";
+			$bad++;
 			continue;
 		} else if ($returnCode == $code && $returnCode != 0) {
 			echo "[OK] Test " . $name . " passed\n";
+			$good++;
 			continue;
 		}
 
@@ -31,9 +36,13 @@
 
 		if ($shouldBe == $is) {
 			echo "[OK] Test " . $name . " passed\n";
+			$good++;
 		} else {
 			echo "[ERR] Test " . $name . " failed\n";
+			$bad++;
 			echo shell_exec("diff " . $testDir . "results/" . $xmlName . " " . $tmpDir . $xmlName);
 		}
 	}
+
+	echo "\nTotal passed/errors >> " . $good . "/" . $bad . " <<\n";
 ?>
