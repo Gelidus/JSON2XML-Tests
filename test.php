30d9ea1a1dd07632f161aff8063da124f1850d50
Gelidus <gelidus@hotmail.sk>
2015-03-10 11:52:30 +0100

    Reformat to spaces

--- test.php
+++ test.php
@@ -1,54 +1,54 @@
 <?php
-	$script = "../jsn.php";
-	$testDir = getcwd() . "/";
-	$tmpDir = $testDir . "tmp/";
+  $script = "../jsn.php";
+  $testDir = getcwd() . "/";
+  $tmpDir = $testDir . "tmp/";
 
-	$tests = array_diff(scandir($testDir . "tests"), array('..', '.'));
-	$results = array_diff(scandir($testDir . "results"), array('..', '.'));
-	include($testDir . "commands.php");
+  $tests = array_diff(scandir($testDir . "tests"), array('..', '.'));
+  $results = array_diff(scandir($testDir . "results"), array('..', '.'));
+  include($testDir . "commands.php");
 
-	$cleanup = $argv[1] == "clean";
+  $cleanup = $argv[1] == "clean";
 
-	$total = count($commands);
-	$good = 0; $bad = 0;
+  $total = count($commands);
+  $good = 0; $bad = 0;
 
-	foreach ($commands as $name => $opts) {
-		$jsonName = $name . ".json";
-		$xmlName = $name . ".xml";
-		$code = $opts[0];
-		$options = $opts[1];
+  foreach ($commands as $name => $opts) {
+    $jsonName = $name . ".json";
+    $xmlName = $name . ".xml";
+    $code = $opts[0];
+    $options = $opts[1];
 
-		$paths = " --input=" . $testDir . "tests/" . $jsonName . " --output=" . $tmpDir . $xmlName;
-		$cmd = "php -d open_basedir=\"\" " . $script . $paths . " " . $options; # get options from command
+    $paths = " --input=" . $testDir . "tests/" . $jsonName . " --output=" . $tmpDir . $xmlName;
+    $cmd = "php -d open_basedir=\"\" " . $script . $paths . " " . $options; # get options from command
 
-		$errOutput = exec($cmd, $execOutput, $returnCode);
+    $errOutput = exec($cmd, $execOutput, $returnCode);
 
-		if ($returnCode != $code) { # code from commands
-			echo "[ERR] Test " . $name . " failed with code: " . $returnCode . " expected: " . $code . "\n";
-			$bad++;
-			continue;
-		} else if ($returnCode == $code && $returnCode != 0) {
-			echo "[OK] Test " . $name . " passed\n";
-			$good++;
-			continue;
-		}
+    if ($returnCode != $code) { # code from commands
+      echo "[ERR] Test " . $name . " failed with code: " . $returnCode . " expected: " . $code . "\n";
+      $bad++;
+      continue;
+    } else if ($returnCode == $code && $returnCode != 0) {
+      echo "[OK] Test " . $name . " passed\n";
+      $good++;
+      continue;
+    }
 
-		$shouldBe = preg_replace("/\n|\ |\t/", "", file_get_contents($testDir . "results/" . $xmlName));
-		$is = preg_replace("/\n|\ |\t/", "", file_get_contents($tmpDir . $xmlName));
+    $shouldBe = preg_replace("/\n|\ |\t/", "", file_get_contents($testDir . "results/" . $xmlName));
+    $is = preg_replace("/\n|\ |\t/", "", file_get_contents($tmpDir . $xmlName));
 
-		if (strtolower($shouldBe) == strtolower($is)) {
-			echo "[OK] Test " . $name . " passed\n";
-			$good++;
-		} else {
-			echo "[ERR] Test " . $name . " failed\n";
-			$bad++;
-			echo shell_exec("diff " . $testDir . "results/" . $xmlName . " " . $tmpDir . $xmlName);
-		}
+    if (strtolower($shouldBe) == strtolower($is)) {
+      echo "[OK] Test " . $name . " passed\n";
+      $good++;
+    } else {
+      echo "[ERR] Test " . $name . " failed\n";
+      $bad++;
+      echo shell_exec("diff " . $testDir . "results/" . $xmlName . " " . $tmpDir . $xmlName);
+    }
 
-		if ($cleanup) {
-			unlink($tmpDir . $xmlName);
-		}
-	}
+    if ($cleanup) {
+      unlink($tmpDir . $xmlName);
+    }
+  }
 
-	echo "\nTotal passed/errors >> " . $good . "/" . $bad . " <<\n";
+  echo "\nTotal passed/errors >> " . $good . "/" . $bad . " <<\n";
 ?>
